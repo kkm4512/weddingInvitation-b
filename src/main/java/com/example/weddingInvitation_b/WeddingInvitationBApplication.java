47459de5d5382eb9e03945b66ede3f6c75cf3588
kkm4512 <nayoun440@naver.com>
2026-05-03 21:59:30 +0900

    init

---
+++ src/main/java/com/example/weddingInvitation_b/WeddingInvitationBApplication.java
@@ -0,0 +1,13 @@
+package com.example.weddingInvitation_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingInvitationBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingInvitationBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weddingInvitation_b/WeddingInvitationBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weddingInvitation_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingInvitationBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
